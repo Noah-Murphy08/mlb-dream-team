c285eba1f38d560c8d3f615a9568583e76683c12
Noah Murphy <ncm0897@gmail.com>
2024-08-04 20:46:17 -0700

    nav bar and main team page built

--- controllers/teams.js
+++ controllers/teams.js
@@ -6,4 +6,22 @@ const Team = require('../models/team');
 const Player = require('../models/player');
 const Pitcher = require('../models/pitcher');
 
+
+router.get('/', async (req, res) => {
+    try {
+        res.locals.populatedTeams = await Team.find({}).populate('owner');
+        if (req.session.user) {
+            res.locals.userTeams = await Team.find({ owner: req.session.user._id }).populate('owner');
+        } else {
+            res.locals.userTeams = [];
+        }
+        res.render('teams/index.ejs')
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
+
+
 module.exports = router;
